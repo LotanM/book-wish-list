e413eeb99d15eb781558a73b20bab365b1fc8c16
Lotan <lotan.mash@gmail.com>
2021-04-07 14:58:42 +0300

    lotan's commit

---
+++ src/cmps/BookPreview.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+
+export class BookPreview extends Component {
+    state = {
+      books: null
+    }
+  
+    async componentDidMount() {
+     
+    }
+  
+    render() {
+      return (
+          <div class="book-preview">
+              
+          </div>
+      )
+    }
+  }
